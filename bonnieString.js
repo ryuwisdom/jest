e0d509d949ce9a97136979dbaaeb65eb1a7edd0d
ryuwisdom <ryuwisdom@crossangle.io>
2021-04-06 16:54:26 +0900

    wip

--- bonnieString.js
+++ bonnieString.js
@@ -1,47 +1,102 @@
 /*
-input (any) : target value to convert
-output (string) :
-    fail > '-'
+input (string) : target string length to count
+output (number) :
+    fail > -1
     pass > 각 지정값
 
-    오늘의 핵심
-    요구사항 정의 및 확인
+    핵심 : 요구사항 정의 및 확인
     //
  */
 
-let ret = '-';
-// filter NaN (Not a Number), Invalid value
-if (!isNaN(rawValue)) {
-  return ret;
-}
-try {
-  rawValue = String(rawValue);
-} catch {
-  return '-';
-}
 
 exports.bonnieLength = function (rawValue) {
-  // check condition
-  let count = 0;
-  for (element of rawValue) {
-    count++;
-  }
-  return count;
+    let count = -1;
+// filter NaN (Not a Number), Invalid value
+    if (typeof rawValue !== 'string') {
+        return count;
+    }
 
-  //   return [...this].reduce((v) => v + 1, 0);
+    // check condition
+    count = 0;
+    for (let element of rawValue) {
+        count++;
+    }
+    //console.log([...rawValue].length)
+    return count;
+
+    // [...this].reduce((v) => v + 1, 0);
 };
 
-exports.bonnieIndexOf = function (rawValue) {
-  // 11번째에 존재하는 문자열을 넣었을때 해당 index return
-  const result = rawValue.split(',');
-  //   const initValue = 0;
-  const exampleWord = 'With';
-  result.map((v, index) => {
-    console.log(index);
-    console.log(v);
-    return index;
-  });
+/*
+behavior: rawString에서 targetString이 매칭이 되는 index시작점
+input (string, string) : 원본 텍스트, 찾을 대상 텍스트
+output (number) : 찾을 대상의 텍스트의 첫번째 매칭되는 자리수
+    fail > -1
+    pass > 0이상 number
+ */
+exports.bonnieIndexOf = function (rawString, targetString, startIndex = 0) {
+    // 11번째에 존재하는 문자열을 넣었을때 해당 index return
+    function bonnieSubstr(rawString, matchIndex, targetString) {
+        let destString = ''
+        for (let i = matchIndex; i < matchIndex + targetString.length; i++){
+            destString += rawString[i]
+        }
+        return destString == targetString
+    }
 
-  //   result.reduce(function())
-  console.log(result);
+    if(rawString.length < targetString.length){
+        return -1
+    }
+
+    // match target 1st char == raw specific index char
+    // after match, check target string include rawstring from index
+    let matchIndex = -1
+    for (let i = 0; i < rawString.length; i++) {
+
+        const target = targetString[0] // startIndex
+
+        if (target === rawString[i]) {
+            matchIndex = i
+            if(startIndex > matchIndex) {
+                matchIndex = -1
+                continue;
+            }
+
+            if (bonnieSubstr(rawString, matchIndex, targetString)) return matchIndex
+        }
+    }
+    return -1
 };
+
+/*
+behavior: rawString에서 startIndex 에서 endIndex의 범위에 포함하는 문자열 추출
+input (number, number(optional)) : startIndex, endIndex
+- endIndex가 없으면 startIndex부터 rawInex의 length까지의 문자열 추출
+- startIndex가 -1일때 오직 마지막 문자열만 추출
+output (string) : startIndex와 endIndex를 포함하는 문자열
+    fail > ''
+    pass > 지정한 범위에 포함하는 문자
+ */
+
+exports.bonnieSlice = function (rawString, startIndex=0, endIndex) {
+    let result= ''
+    let endIndex = rawString.length
+
+    // startIndex가 0보다 크거나 같을때 rawString의 범위에서
+    // startIndex를 찾고
+    // startIndex부터 endIndex범위를 구하기
+
+    // startIndex가 -1일때  rawString.length index 반환
+    // startIndex가 -1미만일때  '' 반환
+
+    if(startIndex >= 0) {
+     // rawString[startIndex]
+        for(let i =0; rawString.length ;i++){
+
+        }
+
+    }
+
+
+}
+
--- test/bonnieString.test.js
+++ test/bonnieString.test.js
@@ -7,27 +7,33 @@ describe('bonnie string methods', () => {
   it('length ', () => {
     expect(bonnieMethods.bonnieLength('CrossAngleWithBonnie')).toBe(20);
   });
+    it('length ', () => {
+        expect(bonnieMethods.bonnieLength('  ')).toBe(2);
+    });
   it('indexOf ', () => {
-    expect(bonnieMethods.bonnieIndexOf('Xangle App With Harry')).toBe(11); // with가 몇번째
+    expect(bonnieMethods.bonnieIndexOf('Hello world, welcome to the universe.','welcome')).toBe(13); // 인자 With
+  });
+    it('indexOf ', () => {
+        expect(bonnieMethods.bonnieIndexOf('Hello world, welcome to the universe. you are welcome','welcome', 15)).toBe(46); // 인자 With
+    });
+  it('slice ', () => {
+    expect(bonnieMethods.bonnieSlice('Hello world!',3)).toBe('lo world!');
   });
   it('slice ', () => {
-    expect('XangleWithSimon').toBe('WithSimon'); // 인자 두 개
+    expect(bonnieMethods.bonnieSlice('Hello world!',3,8)).toBe('lo wo');
   });
-  it('split ', () => {
-    expect('CoinJab,withJeff').toStrictEqual(['CoinJab', 'withJeff']);
+  it('slice ', () => {
+    expect(bonnieMethods.bonnieSlice('Hello world!',0,1)).toBe('H');
   });
+  it('slice ', () => {
+    expect(bonnieMethods.bonnieSlice('Hello world!',20)).toBe('');
+  });
+  it('slice ', () => {
+    expect(bonnieMethods.bonnieSlice('Hello world!',-1)).toBe('!');
+  });
+
+  // it('split ', () => {
+  //   expect('CoinJab,withJeff').toStrictEqual(['CoinJab', 'withJeff']); // 인자 : ','
+  // });
 });
-// describe('bonnie string methods', () => {
-//   it('length ', () => {
-//     expect('CrossAngleWithBonnie'.bonnieLength()).toBe(20);
-//   });
-//   it('indexOf ', () => {
-//     expect('Xangle App With Harry'.bonnieIndexOf('With')).toBe(11); // with가 몇번째
-//   });
-//   it('slice ', () => {
-//     expect('XangleWithSimon'.bonnieSlice(6)).toBe('WithSimon'); // 인자 두 개
-//   });
-//   it('split ', () => {
-//     expect('CoinJab,withJeff'.bonnieSplit(',')).toBe(['coinJab', 'withJeff']);
-//   });
-// });
+
